ba8dab844b1c29b4eb3ef789ce82d3eb14dd94e0
Haitham <Haithamhany7@gmail.com>
2026-04-06 20:52:22 -0400

    Updating paths and extensions

--- src/segmentation/sam_masks.py
+++ src/segmentation/sam_masks.py
@@ -59,7 +59,7 @@ def run_sam_folder(config):
 
     # Loop through images
     for filename in os.listdir(input_folder):
-        if filename.lower().endswith((".jpg", ".png")):
+        if filename.lower().endswith((".jpg", ".png", ".jpeg")):
 
             input_path = os.path.join(input_folder, filename)
             output_path = os.path.join(output_folder, filename)
